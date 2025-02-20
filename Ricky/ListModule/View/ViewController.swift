2607efa3cbb9e92c596fe0a4710eb9b636833101
CyrilusK <kcyrg17@gmail.com>
2025-02-20 15:17:40 +0300

    initial project

---
+++ Ricky/ListModule/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ricky
+//
+//  Created by Cyril Kardash on 20.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
